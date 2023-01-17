35919c2c26971b78f81c348bda47d58844f3ec0f
Bruno Saraiva <bruno.msaraiva2@gmail.com>
2023-01-17 19:39:29 +0000

    bugfixing github actions

--- setup.py
+++ setup.py
@@ -38,6 +38,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/brunomsaraiva/pyx2pxd',
-    version='0.0.3',
+    version='0.0.4',
     zip_safe=False,
 )
