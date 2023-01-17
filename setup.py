a75b24bda9d548ce74ccbcf1a0f5e6b61598a936
Bruno Saraiva <bruno.msaraiva2@gmail.com>
2023-01-17 14:58:25 +0000

    project cleanup and added github workflow

--- setup.py
+++ setup.py
@@ -4,12 +4,6 @@
 
 from setuptools import setup, find_packages
 
-with open('README.rst') as readme_file:
-    readme = readme_file.read()
-
-with open('HISTORY.rst') as history_file:
-    history = history_file.read()
-
 requirements = [ ]
 
 test_requirements = ['pytest>=3', ]
@@ -36,7 +30,6 @@ setup(
     },
     install_requires=requirements,
     license="MIT license",
-    long_description=readme + '\n\n' + history,
     include_package_data=True,
     keywords='pyx2pxd',
     name='pyx2pxd',
