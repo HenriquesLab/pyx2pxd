ab892258723568e42eaf70c26cdab177047b5c88
Bruno Saraiva <bruno.msaraiva2@gmail.com>
2023-01-18 09:53:21 +0000

    added pytest to github actions and bumped version

--- setup.py
+++ setup.py
@@ -38,6 +38,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/brunomsaraiva/pyx2pxd',
-    version='0.0.5',
+    version='0.1.0',
     zip_safe=False,
 )
