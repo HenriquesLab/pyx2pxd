6a7d6d5f0a55ebe3e9d67d03259e3a7a951a914e
Bruno Saraiva <bruno.msaraiva2@gmail.com>
2023-02-10 11:35:22 +0000

    updated pytest to autogenerate and then delete a pyx file

--- tests/test_pyx2pxd.py
+++ tests/test_pyx2pxd.py
@@ -4,25 +4,25 @@
 
 import pytest
 import os
-
-
 from pyx2pxd.pyx2pxd import autogenerate_pxd_files
 
 
-@pytest.fixture
-def sanbox_path() -> str:
-    """
-    Get the sandbox directory for testing
-    """
-    path = os.path.join(os.path.dirname(__file__), "..", "sandbox")
-    return path
-
-
-def test_pyx2pxd(sanbox_path: str):
+def test_pyx2pxd():
     """
     Test the pyx2pxd function
     """
-    autogenerate_pxd_files(sanbox_path)
-    assert os.system("cythonize -i " + os.path.join(sanbox_path, "*.pyx")) == 0
+    path = os.path.dirname(os.path.realpath(__file__))
+    func_name = "cdef int test_pyx2pxd():\n"
+    file = open(os.path.join(path, "testpyx.pyx"), "w")
+    lines_to_write = "# cython: autogen_pxd=True\n"
+    lines_to_write += "\n"
+    lines_to_write += func_name
+    lines_to_write += "    return 1"
+    file.writelines(lines_to_write)
+    file.close()
+    autogenerate_pxd_files(path)
+    assert func_name[:-2] in open(os.path.join(path, "testpyx.pxd"), "r").readlines()[-1]
+    os.remove(os.path.join(path, "testpyx.pyx"))
+    os.remove(os.path.join(path, "testpyx.pxd"))
     
 
